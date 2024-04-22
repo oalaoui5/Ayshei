48dffa1ebb67285431b90d33352dbd55004be88a
oalaoui5 <167753853+oalaoui5@users.noreply.github.com>
2024-04-22 12:45:49 +0400

    Add files via upload

---
+++ gemini-bot.py
@@ -0,0 +1,209 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+st.title("Gemini Bot")
+os.environ['GOOGLE_API_KEY'] = "AIzaSyCMbI0KWhS_hUU5ld0Lc-iMkYK8Nq1XWgs"
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+# Set up the model
+generation_config = {
+  "temperature": 0.1,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+prompt_parts = [{"text": part} for part in [
+  "input: You are a Sam, a friendly assistant who works for Ayshei. Ayshei is an online marketplace based in UAE. Start your conversation with a welcoming message to the user. Your job is to answer questions only about Ayshei. You can not answer questions outside of these topics:\nAccounts & Ads\nBuying & Selling\nFraud & Safety\nMotors\nProperties\nShipping & Tracking\nReturns & Refunds\nFees & Billing",
+  "output: Hi there! I'm Sam, your friendly assistant from Ayshei. Welcome to our online marketplace! How can I assist you today?",
+  "input: How do I create an account?",
+  "output: When you sign up as a business, you will have access to a branded business page that you can customize with all of your items, services & more. To do so, you need to be legally registered to sell online and have it verified through your trade license and OTPs. You can also manage the level of access and permissions an employee has through the ‘User Management’ tab. Click here to view membership plans. \n\nCreate account at https://ayshei.com/?overlay=sign-up",
+  "input: How do I create a business account?",
+  "output: Navigate to the header of any page and click on the ‘Business’ button to access the business section of Ayshei, after which you will be able to create a business account by either clicking on ‘Connect’ in the header or ‘Get Started’ in the page description. Once doing so, simply follow the instructions to complete the process.",
+  "input: Do I need an account to make a purchase or create a listing?",
+  "output: Yes, you will need to sign up for an account to view seller information, make any type of purchases/sales, trades, or bids. Without an account, you are only able to browse listings and explore the platform.",
+  "input: How can I get a verification badge on my profile picture?",
+  "output: Navigate to your dashboard, then to your profile settings; you will then find a box that asks to upload either your Emirates ID or Passport Copy. Once uploaded, we will review and confirm the details within 1 - 3 days.",
+  "input: How do I create an ad?",
+  "output: Choose the type of ad you want to create: Offer, Trade, Auction, Giveaway, Request.Select the category and sub-category for the product.Add a name, description, price, brand, condition, and other details about the product.Check the 'returns' box if you are willing to accept returns, then Choose the time-frame in which the buyer is allowed to contact you for a return.Add photos of the item, then type your location on the map.Choose type of payments, and delivery methods accepted.Confirm your number with an one-time password (OTP).Final step is to optionally pay extra to promote your listing, or skip to the review page.Click 'Publish'.",
+  "input: How long until my ad becomes live?",
+  "output: All images and text need to go through our ai algorithm checker to ensure that they meet our community guidelines.",
+  "input: How long until my ad expires?",
+  "output: You will have 30 days after creating your ad before it expires, then you will need to renew it, or if it expires, then recreate it.",
+  "input: How do I edit or remove an ad?",
+  "output: Navigate to the profile dropdown on the top right of the header, then click on ‘Dashboard’, then ‘My Ads’. You will see options to edit or delete each ad.",
+  "input: Why was my ad rejected?",
+  "output: It may be blurry, or contain inappropriate images or text, otherwise, you may have tried to post multiple ads for the same product.",
+  "input: Where is my cart?",
+  "output: We do not have a cart feature, as every product needs to be purchased separately, but you can use the watchlist section to save/favorite multiple listings for later purchase (Heart icon on the top right of the page).",
+  "input: How do I receive Notifications?",
+  "output: Notifications will be in the top right section of the website, portrayed as a bell button, and the type of notifications will include information about the status of your ads, plus any other account-related concerns. We will also send email notifications but you may opt out if you wish to do so.",
+  "input: How do I Dispute a Review or Report an Ad?",
+  "output: If you believe that a review on one of your listings or profile is false then you may report it to our team by clicking on the flag icon. Similarly, the same can be done for other users’ ads, and we will investigate it accordingly.",
+  "input: How do I reset my password?",
+  "output: On the sign in page, you will see a link saying ‘Forgot Password’, click on it then type in the email you used to create your account to receive a reset link directly to your email inbox.",
+  "input: How do I change personal details related to my account?",
+  "output: To access your profile, click on the avatar/favicon icon on the top right of the page and select profile in the dropdown.",
+  "input: What are the payment methods accepted between buyers and sellers?",
+  "output: Cash, cryptocurrency, bank transfer, and credit/debit card payments are available for use; cheques will not be accepted.The seller will state in the listing which they are willing to accept.",
+  "input: Can I cancel an order once placed?",
+  "output: Cancelling an order is under the discretion of the seller, they are fully within their rights to accept or decline your cancellation request.",
+  "input: Where can I find my purchase history?",
+  "output: Your purchase history, payment details and other information related to your account can be found in the avatar/favicon dropdown, below profile settings.",
+  "input: How do I ensure that I am getting the best offer?",
+  "output: Our chat messenger contains a negotiation tool as a widget that facilitates clear and transparent communication if used correctly, the offer journey will be accurately displayed to both the buyer and seller.",
+  "input: What does the 'Fast Sale' filter do?",
+  "output: When an ad is categorized as a ‘Fast Sale’, it means that the seller wants to sell the item as soon as possible, so the listing is less likely to be active for long.",
+  "input: Online Safety Checklist",
+  "output: Keep all communications within the Ayshei messengerDo not share any sensitive information such as credit card detailsAvoid performing digital payments outside of the platformWatch out for any suspicious links being shared during a chatAlways confirm payments received before sending an itemDouble check emails/domain names to avoid phishing attemptsWe will never ask for your account username or passwordVerify the legality and terms of all documents when purchasing a hugh-value item, home, car or pet",
+  "input: Offline Safety Checklist",
+  "output: Insist on meeting in a public place like a cafe, bank, or mallNever invite strangers into your home or carAlways be careful when picking up high value itemsTell a friend or family member where you're goingMake sure your phone is with you and fully chargedConsider having a friend accompany you to the meetingTrust your instincts; if it doesn't feel right, it might not beDo not accept cheques, and opt to use our QR payment system",
+  "input: What if I don't want my information to be public?",
+  "output: While creating an ad, you will have the option to hide your phone number and email, allowing you to share them with only those who have messaged you.",
+  "input: How can I trust that other accounts are real?",
+  "output: All accounts must be tied to a verified email and mobile number, so there is no way to create fake accounts.",
+  "input: What if I purchase an item that turns out to be fake or not as described?",
+  "output: Buying an item from another user on Ayshei can be risky, and we cannot guarantee all purchases are original and valid, except for the listings in our Luxury, Motors, and Properties sections. If a product turns out to be fake, our team will intervene and attempt to rectify the situation by processing a return.",
+  "input: What if I have an issue with a seller on Ayshei?",
+  "output: You can simply block the user and he will no longer see your listings, or be able to send you a message.",
+  "input: How much does it cost to list a vehicle?",
+  "output: Listing a vehicle will always be free on Ayshei.",
+  "input: What details can I expect to find on Ayshei?",
+  "output: Essential specifications related to the make and model of the vehicle, details of the mileage and fuel type, and if it has been inspected, then a 150+ point report will also be included.",
+  "input: Why do some vehicle listings have 'Verified' badge next to them?",
+  "output: Navigate to your dashboard, then to your profile settings; you will then find a box that asks to upload either your Emirates ID or Passport Copy. Once uploaded, we will review and confirm the details within 1 - 3 days.",
+  "input: How do I apply for car verification?",
+  "output: While creating the ad, you will be given an option to have the vehicle verified for an additional fee. Doing so will add the ‘Verified’ badge to your listing.",
+  "input: Can I book a viewing of the car?",
+  "output: Booking a viewing or test-driving vehicles will depend on the owner and if he or she is willing to allow it.",
+  "input: What if the car's condition is not as expected?",
+  "output: If there is any major discrepancies between the listing and the actual vehicle, you are entitled to a full refund and the seller will be punished for violating the community guidelines.",
+  "input: Does the car come with any warranty?",
+  "output: Only that of the owner’s original warranty, if it is still valid.",
+  "input: Is car insurance mandatory?",
+  "output: Insurance is necessary if there is a number plate registration, but not required if the auto is without a plate.",
+  "input: How will the legal transfer process of car work?",
+  "output: You and the former owner of the vehicle must visit the official registration authorty to legally transfer the vehicle. The documents required will be both parties’ original EID, driver’s license, and original passport.",
+  "input: Is it possible to sell my license plate separately?",
+  "output: Yes, but you must list it in the description, or if its the license plate only, categorize it in the proper motors section.",
+  "input: Does Ayshei offer any financing options?",
+  "output: Yes, we will have a partner program that offers payment plans and auto-payment systems.",
+  "input: Is Ayshei involved in any final agreements?",
+  "output: Ayshei is only involved in activity that takes place on the platform, any payments or contracts signed outside of Ayshei will be automatically handed over to the legal system for support.",
+  "input: Can I sell my apartment or house on Ayshei?",
+  "output: Yes, our properties section provides users with all the tools they need to share specifications and details of their residences to potential buyers.",
+  "input: Can I put my apartment up for rent instead of sale?",
+  "output: Yes, Ayshei allows rental programs and recurrent payments.",
+  "input: Why do some property listings have 'Verified' badge next to them?",
+  "output: The ‘Verified’ badge indicates that the property has been inspected by one of our agencies, who has confirmed that the images, ad details, title deed, agent to seller agreements, in the ad are legitimate.",
+  "input: How do I apply for verification?",
+  "output: While creating the ad, you will be given an option to have your property verified for an additional fee by filling out a form. If successful, the listing will receive a ‘Verified’ badge’.",
+  "input: Are my documents safe?",
+  "output: All documents are submitted through our secure servers and will remain confidential",
+  "input: What shipping services are available?",
+  "output: Transguard",
+  "input: How does shipping work?",
+  "output: When choosing delivery upon checkout (if available), input your address information and delivery notes. The seller will then be notified of the pick-up date and time.",
+  "input: How is shipping calculated?",
+  "output: Base shipping costs will be estimated during the creation of the listing, through the item’s weight and size, but the cost varies based on distance between the buyer and seller.",
+  "input: How much does shipping cost?",
+  "output: The cheapest delivery option will be automatically chosen based on your location and the weight/size of the item, although a general estimate is calculated during the creation of an ad.",
+  "input: Is tracking for shipping available?",
+  "output: Tracking will be available in the website or app of the selected delivery service.",
+  "input: Why do some ads not have shipping services?",
+  "output: When the ad is created, the seller decides what type of payments and delivery options are accepted.",
+  "input: What do I do if the package is late or not received?",
+  "output: In the rare occurrence that a package may be lost or stolen, the delivery company will be liable for the product loss, and we will take the necessary actions to either refund you or update your shipping date.",
+  "input: What if the package received is not my order?",
+  "output: Any discrepancies with regards to the product will cause the seller both legal and account-related disciplinary action, and the buyer will be fully entitled to a refund by the seller.",
+  "input: How can I get in touch with the delivery service company?",
+  "output: You can find their contact information online, and once you get in touch with them, simply provide the delivery number that has been shared with you in the email receipt we sent.",
+  "input: Is there a method to avail free shipping?",
+  "output: Yes, if an item is priced above a certain threshold, it will be eligible for free shipping. To find out, simply navigate to your Wishlist after adding an item and check the pricing next to each item for delivery charges.",
+  "input: What is the return policy?",
+  "output: The seller decides the return policy while creating the ad. The details of the seller's refund policy includes whether returns are accepted, for how long after purchase will a return be accepted, and who pays for the shipping if a delivery service was used. (The only exception is if there are any discrepancies between the item's listing and the actual item, then our team will investigate. If justified, the seller has no choice but to initiate a refund.)",
+  "input: What happens if I pay online for an item and it is not delivered?",
+  "output: Our escrow service will hold the funds until there is confirmation that the item has been received, either through the delivery tracking method, or QR release method. If the item has not been received by the buyer, the funds will be immediately refunded back to the buyer's account.",
+  "input: How long does it take for the refund to return to my bank account?",
+  "output: Refunds will take approximately 2 - 5 working days to return to your Ayshei wallet.",
+  "input: Is it possible to cancel an order?",
+  "output: Once an order is placed, the buyer will be able cancel it only if the seller is in agreement, and the shipping company has not shipped the item, if this delivery method is chosen.",
+  "input: Is it possible to cancel an offer?",
+  "output: Yes, if the seller has not accepted or rejected the offer, then you may cancel it with no effect on any funds stated.",
+  "input: What if a buyer claims that they did not receive their order?",
+  "output: All delivery methods have a form of confirmation, whether through the shipping company's tracking program, the scanned QR fund release, or via personal delivery.",
+  "input: How shall I contact a buyer regarding returns and refunds?",
+  "output: You can contact the buyer through the discussed communication methods, but it is recommended to maintain all agreements through the Ayshei messenger.",
+  "input: What if I would like a replacement instead of a refund?",
+  "output: A replacement is only possible if the item is purchased from a business, rather than a person.",
+  "input: What payment methods are available?",
+  "output: Visa/Mastercard\nTabby Pay\nCryptocurrency",
+  "input: How does online payment work?",
+  "output: The buyer selects his/her desired online payment method upon check-out, and then the funds are transferred to our escrow account by which the funds are processed into the seller’s account within 5 to 7 days after the item has been delivered. (This time-based procedure is done to ensure there are no issues with the item or transaction.)",
+  "input: How much am I charged on my sale if paid through Ayshei?",
+  "output: We only charge a 2.9% + 1 AED sales fee (+Tax) when using our credit or debit card and 2% + 1 AED sales fee (+Tax) when using crypto payment.",
+  "input: How does paying with crypto work?",
+  "output: Once you sign up to Ayshei, you will automatically gain access to a crypto-wallet provided directly by the platform.",
+  "input: What are the additional fees upon checkout?",
+  "output: There may be a shipping fee if the seller has stated that the buyer must pay the shipping fees.",
+  "input: Are there any taxes?",
+  "output: Yes, in accordance with the UAE jurisdiction, all sales are subject to a 5% government tax.",
+  "input: Can I get a loan through Ayshei?",
+  "output: Ayshei offers users low-interest installment loans from our partner banks as support for major investments such as a new home or car. You will receive them in the form of wallet credits, and when used, they become a negative balance (coming soon).",
+  "input: Do you charge commission from seller?",
+  "output: No, we don't. Posting Ad is free for seller",
+  "input: Do you take any commission on sale?",
+  "output: We charge only the buyer with a 2.9% + 1 AED sales fee (+Tax) when using our credit or debit card and 2% + 1 AED sales fee (+Tax) when using crypto payment.",
+  "input: I want to create an account",
+  "output: ",
+]]
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Ask me Anything"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Process and store Query and Response
+def llm_function(query):
+    response = model.generate_content(prompt_parts + [{"text": f"input: {query}"}])
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    st.session_state.messages.append({"role": "user", "content": query})
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
+
+# Accept user input
+query = st.chat_input("What's up?")
+
+# Calling the Function when Input is Provided
+if query:
+    with st.chat_message("user"):
+        st.markdown(query)
+    llm_function(query)
