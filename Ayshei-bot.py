13ee32c739964ea840482231f6e6093d5bc610d7
oalaoui5 <167753853+oalaoui5@users.noreply.github.com>
2024-04-22 18:14:46 +0400

    Update Ayshei-bot.py

--- Ayshei-bot.py
+++ Ayshei-bot.py
@@ -331,7 +331,9 @@ prompt_parts = [{"text": part} for part in [
 "support@ayshei.com"
 
 "Input: call center phone number "  
-"Output: 800-AYSHEI, 800 297 434" 
+"Output: "
+"800-AYSHEI
+"800 297 434" 
 "From Mon - Sun from 9 AM to 9 PM "
 ]]
 
