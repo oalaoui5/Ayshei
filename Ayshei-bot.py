00965157ce3f65553f82c928c26af57b64e55bd1
oalaoui5 <167753853+oalaoui5@users.noreply.github.com>
2024-05-07 17:53:09 +0400

    Update Ayshei-bot.py

--- Ayshei-bot.py
+++ Ayshei-bot.py
@@ -205,7 +205,7 @@ for message in st.session_state.messages:
 def llm_function(query):
     response_text = ""  # Initialize response_text with an empty string
 
-    search_keywords = ["searching for an", "search for a", "find", "look for", "look for a", "look for an", "show me", "list", "display", "looking for a", "looking for an"]
+    search_keywords = ["searching for an", "search for a", "find", "look for", "look for a", "show me", "list", "display", "looking for a"]
     if any(keyword in query.lower() for keyword in search_keywords):
         search_query = query.lower()
         for keyword in search_keywords:
